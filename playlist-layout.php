7729f4f7df136784d8c3ff5173acf5bfcca03892
dreaddy <dreaddymck@gmail.com>
2018-04-09 18:39:39 -0500

    without sensitive data

---
+++ playlist-layout.php
@@ -0,0 +1,53 @@
+<div class="player row">
+
+	<div class="col-md-4">
+
+		<div class="panel panel-box">
+			<div class="panel-body">
+				<div class="cover "></div>
+			</div>
+			<div class="panel-heading options">			
+
+				<div class="controls row">
+					<div class="col-xs-3">
+						<i class="btn btn-default play fa fa-play-circle"  aria-hidden="true" title="Play"></i>
+						<i class="btn btn-default pause fa fa-pause hidden"  aria-hidden="true" title="Pause"></i>							
+					</div>
+					<div class="col-xs-3">
+						<i class="btn btn-default rew fa fa-step-backward" aria-hidden="true" title="Back"></i>							
+					</div>
+					<div class="col-xs-3">
+						<i class="btn btn-default fwd fa fa-step-forward"  aria-hidden="true" title="Forward"></i>							
+					</div>
+					<div class="col-xs-3">
+						<i class="btn btn-default showlist fa fa-list"  aria-hidden="true"  title="Tone Deaf Playlist"> </i> 								
+					</div>			
+				</div> 
+
+			</div>
+
+			
+		</div>	
+
+	</div>
+	<div class="col-md-8">
+		
+		<h3 class="title" title="click for more information"></h3>
+
+		<div class="volume" style="display:none"></div>
+		
+		<div class="duration">
+			
+			<h1 class="artist"></h1>
+		
+		</div>
+		
+		<div class="tracktime">0 / 0</div>
+	</div>
+
+</div>
+<div class="row">
+	<ul class="playlist hidden">
+		<li audiourl="*.mp3" cover="cover1.jpg" artist="Artist 1">*.mp3</li>
+	</ul>
+</div>
