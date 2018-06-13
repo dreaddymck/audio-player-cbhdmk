a6f5097f385682a5adc7b6403554901a6b406e23
dreaddy <dreaddymck@gmail.com>
2018-06-12 19:22:43 -0500

    ...

--- index.php
+++ index.php
@@ -113,7 +113,7 @@ if (!class_exists("WPAudioPlayerCBHDMK")) {
 					$twitter_type	= "player";
 
 					echo '<meta name="twitter:player" content="'.$url.'" />'."\r\n";
-					echo '<meta name="twitter:player:streamr" content="'.$matches[0].'" />'."\r\n";
+					echo '<meta name="twitter:player:stream" content="'.$matches[0].'" />'."\r\n";
 				}
 			}
 			if( is_archive()  ){
