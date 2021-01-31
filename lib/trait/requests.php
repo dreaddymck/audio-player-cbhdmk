445f22a60d3756de88fae7ecb1290d61da67b3c1
dreaddy <dreaddymck@gmail.com>
2021-01-31 09:42:26 -0500

    Playlist thumbail changes. If no post thumbnail, return first image in content or default thumbnail set in admin

--- lib/trait/requests.php
+++ lib/trait/requests.php
@@ -1,44 +1,44 @@
 <?php
 trait _requests {
-  
+
 	function __construct(){}
-	function handle_requests($data){ return $this->requests($data); }	
-    function requests($data){		
+	function handle_requests($data){ return $this->requests($data); }
+    function requests($data){
 		$response = "{}";
 		$opt = $data;
-		if( is_object($opt) ){ $opt = $data['option']; }	
+		if( is_object($opt) ){ $opt = $data['option']; }
         switch ($opt) {
 			case "search":
 				$response = $this->param_request($data);
-				break;				
+				break;
             case "get":
                 $response = $this->accesslog_activity_get();
                 break;
             case "get_week":
                 $response = $this->accesslog_activity_get_week();
-                break;    
+                break;
             case "get_month":
                 $response = $this->accesslog_activity_get_month();
-				break;  
+				break;
 			case "get_my_ip":
 				$response = $this->get_my_ip();
-				break; 				                              
+				break;
             default:
         }
         return $response;
-    }	
+    }
 	function obj_request($obj) {
-		global $wpdb;		
+		global $wpdb;
 		$args = array(
 			's'					=> !empty($obj->s) ? "/".urldecode($obj->s)  : "",
 			'posts_per_page' 	=> !empty($obj->posts_per_page) ? $obj->posts_per_page : -1,
 			'post_status'      	=> !empty($obj->publish) ? $obj->publish  : "publish",
 			'orderby'          	=> !empty($obj->orderby) ? $obj->orderby  : "",
 			'order'            	=> !empty($obj->order) ? $obj->order  : "",
-			'tag'				=> !empty($obj->tag) ? $obj->tag  : "", 
+			'tag'				=> !empty($obj->tag) ? $obj->tag  : "",
 			'tag__in' 			=> !empty($obj->tag_in) ? array( $obj->tag_in ) : "", //array (id)
 			'tag__not_in'		=> !empty($obj->tag_not_in) ? array( $obj->tag_not_in ) : "", //array (id)
-			'tag_slug__and'		=> !empty($obj->tag_slug__and) ? array( $obj->tag_slug__and ) : "",	
+			'tag_slug__and'		=> !empty($obj->tag_slug__and) ? array( $obj->tag_slug__and ) : "",
 		);
 		return $this->_requests_get_posts($args);
 	}
@@ -54,16 +54,16 @@ trait _requests {
 			'tag'				=> !empty($params["tag"]) ? $params["tag"] : "",
 			'tag__in' 			=> !empty($params["tag_in"]) ? $params["tag_in"] : "",
 			'tag__not_in'		=> !empty($params["tag_not_in"]) ? $params["tag_not_in"] : "",
-			'tag_slug__and'		=> !empty($params["tag_slug__and"]) ? $params["tag_slug__and"] : "",						
+			'tag_slug__and'		=> !empty($params["tag_slug__and"]) ? $params["tag_slug__and"] : "",
 		);
-		return $this->_requests_get_posts($args);			
+		return $this->_requests_get_posts($args);
 	}
 	function _requests_get_posts($args){
 		$posts 	    = get_posts( $args );
 		$response   = $this->render_elements($posts);
 		wp_reset_postdata();
 		return($response);
-	}	
+	}
 	function query($sql){
 		if($this->debug){
 			$this->_log($sql);
@@ -74,12 +74,12 @@ trait _requests {
 		$results    = array();
 		if( $resp instanceof mysqli_result ){ $results = mysqli_fetch_all($resp); }
 		$conn->close();
-		return $results;	
+		return $results;
 	}
 	function top_count_get(){
 		/**
 		 * playlist generated from access log data
-		 */		
+		 */
 		$json = json_decode($this->accesslog_activity_get(), true);
 		if(!$json){return array();}
 		usort($json, function($a, $b) {
@@ -88,24 +88,24 @@ trait _requests {
 			return ($b["time"] < $a["time"]) ? -1 : 1;
 		});
 		$json = array_slice($json,0,10);
-		foreach($json as $key=>$value) { 
-			
+		foreach($json as $key=>$value) {
+
 			$param = (object) array('s' => $value["name"]);
 			$p = json_decode($this->obj_request( $param ));
 
 			$title = !empty($p[0]->title) ? $p[0]->title : urldecode($value["name"]);
 			$date = date('m/d/Y h:i:s a', $json["time"]);
-			
+
 			$json[$key]["title"] = $title;
 			$json[$key]["date"] = $date;
-			$json[$key]["ID"] = $p[0]->ID;						
+			$json[$key]["ID"] = $p[0]->ID;
 			$json[$key]["mp3"] = $p[0]->mp3;
 			$json[$key]["cover"] = $p[0]->cover;
 			$json[$key]["artist"] = $p[0]->artist;
 			$json[$key]["permalink"] = $p[0]->permalink;
 			$json[$key]["wavformpng"] = $p[0]->wavformpng;
 			$json[$key]["tags"] = $p[0]->tags;
-			$json[$key]["moreinfo"] = $p[0]->moreinfo;			
+			$json[$key]["moreinfo"] = $p[0]->moreinfo;
 		}
 		return $json;
 	}
@@ -117,45 +117,45 @@ trait _requests {
 		if( empty( get_option("playlist_config") ) ){ return; }
 		$arr["playlist_json"] = json_decode(get_option("playlist_config"));
 		$arr["top_10_json"] = $this->top_count_get();
-		return $arr;		
+		return $arr;
 	}
-	function render_elements($posts) {		
+	function render_elements($posts) {
 		$response = [];
-		$is_secure = $this->isSecure();		
-		foreach ( $posts as $post ) : setup_postdata( $post );		
-		
+		$is_secure = $this->isSecure();
+		foreach ( $posts as $post ) : setup_postdata( $post );
+
 			if(get_post_status($post->ID) != "publish" ){ continue; }
 
 			$object 	= new stdClass();
-			$audio 		= $this->fetch_audio_from_string( $post->post_content );		
+			$audio 		= $this->fetch_audio_from_string( $post->post_content );
 			if(empty($audio[0])) { continue; }
-			$audio[0] = urldecode($audio[0]);			
-			if($this->path) {					
+			$audio[0] = urldecode($audio[0]);
+			if($this->path) {
 				/* If $this->path exist
 				* playlist_elements call
 				* we should be extracting a single element that matches
 				*/
-				if (strpos(  $audio[0], $this->path) === false) { continue; }				
-			}				
+				if (strpos(  $audio[0], $this->path) === false) { continue; }
+			}
 			$object->ID		        = $post->ID;
-			$object->mp3		    = $audio[0];				
+			$object->mp3		    = $audio[0];
 			$object->wavformpng		= get_post_meta( $post->ID, 'dmck_wavformpng', true );
 			$object->wavformpng	    = $object->wavformpng ? $object->wavformpng : $this->waveformpng($audio[0]);
-			$object->wavformjson	= $this->waveformjson($audio[0]);			
+			$object->wavformjson	= $this->waveformjson($audio[0]);
 			if($is_secure){
 				$object->mp3		    = preg_replace("/^http:/i", "https:", $object->mp3);
 				$object->wavformpng	    = preg_replace("/^http:/i", "https:", $object->wavformpng);
-				$object->wavformjson	= preg_replace("/^http:/i", "https:", $object->wavformjson);	
-			}				
+				$object->wavformjson	= preg_replace("/^http:/i", "https:", $object->wavformjson);
+			}
 			$object->title		= esc_attr($post->post_title);
 			$object->artist		= "dreaddymck";
 			$object->rating		= 0;
-			$object->cover		= $this->fetch_the_post_thumbnail_src( get_the_post_thumbnail($post->ID, "thumbnail") );
+			$object->cover		= $this->fetch_the_post_thumbnail_src( $post );
 			$object->permalink	= get_permalink( $post->ID );
 			$object->moreinfo	= get_option('moreinfo') ? get_option('moreinfo') : "";
 			$object->playlist_thumb = $object->cover;
 			$object->tags 		=  implode( ', ', wp_get_post_tags( $post->ID, array( 'fields' => 'names' )) );
-		
+
 			$excerpt_tmp 	= $post->post_content;
 			$excerpt_tmp 	= htmlspecialchars_decode($excerpt_tmp);
 			$excerpt_tmp 	= preg_replace('#<[^>]+>#', ' ', $excerpt_tmp);
@@ -163,14 +163,14 @@ trait _requests {
 			$excerpt_tmp 	= str_replace( chr( 194 ) . chr( 160 ), ' ', $excerpt_tmp );
 			$excerpt_tmp 	= preg_replace( '#(Sorry, your browser doesn\'t support HTML5 audio\.)#', ' ', $excerpt_tmp );
 			$excerpt_tmp 	= preg_replace( '#(download)#', ' ', $excerpt_tmp );
-	
+
 			$object->excerpt = wp_trim_words( esc_attr( $excerpt_tmp ), 12, "...");
-		
+
 			array_push( $response, $object );
-	
-		endforeach;			
-	
-		return json_encode($response);	
+
+		endforeach;
+
+		return json_encode($response);
     }
 	function waveformpng($str) {
 		return preg_replace('/\.mp3$/', '.wavform.png', $str);
@@ -178,12 +178,27 @@ trait _requests {
 	function waveformjson($str) {
 		return preg_replace('/\.mp3$/', '.wavform.json', $str);
 	}
-	function fetch_the_post_thumbnail_src($img)
+	function fetch_the_post_thumbnail_src($post)
 	{
-		return (preg_match('~\bsrc="([^"]++)"~', $img, $matches)) ? $matches[1] : esc_attr( get_option('default_album_cover') ); //'https://dl.dropboxusercontent.com/u/1273929/MUSIC/FEATURING/photo.jpg';
+		$img = get_the_post_thumbnail_url($post->ID, "thumbnail");
+		if(!$img){
+			$dom = new DOMDocument();
+			libxml_use_internal_errors(true);
+			$dom->loadHTML($post->post_content);
+			$xpath = new DOMXpath($dom);
+			$src = $xpath->query("//img/attribute::src");			
+			if($src->length){
+				foreach( $src as $s ) {
+					$img = $s->nodeValue;
+					break;
+				}
+			}
+		}
+		// return (preg_match('~\bsrc="([^"]++)"~', $img, $matches)) ? $matches[1] : esc_attr( get_option('default_album_cover') );
+		return $img ? $img : esc_attr( get_option('default_album_cover'));
 	}
 	function fetch_audio_from_string($str) {
-		
+
 		$matches = array();
 
 		// This REGEX solution fails on classic html pages/posts imported from blogspot
@@ -196,7 +211,7 @@ trait _requests {
 		// 		'pcm', 'tta', 'wav', 'wma',  'wv',
 		// 	);
 		//	$formats = join( '|', $suffixes );
-		// $formats = "mp3";		
+		// $formats = "mp3";
 		// $regex   = '~
 		// (([^"\'])|^)            # start of string or attribute delimiter -> match 1
 		// (https?                 # http or https
@@ -209,13 +224,13 @@ trait _requests {
 		// (([^"\'])|$)?           # end of string or attribute delimiter -> match 5
 		// ~imUx';                 # case insensitive, multi-line, ungreedy, commented
 		//
-		
+
 		//
 		// $regex   = '~(https?://.+/.+\.(' . $formats . '))(([^"\'])|$)?~imUx';
 		// $regex = '~((?:\'|")https?://\w.+\.(mp3)(?:\'|"))~';
 		// preg_match_all( $regex, $str, $matches, PREG_PATTERN_ORDER );
-	
-		
+
+
 		$dom = new DOMDocument();
 		libxml_use_internal_errors(true);
 		$dom->loadHTML($str);
@@ -226,7 +241,7 @@ trait _requests {
 				$value = $s->nodeValue;
 				array_push($matches,$value);
 			}
-		}		
+		}
 		return $matches;
-	}    
+	}
 }
