132027b6dcf040ac3c3975f3374bec9959a47969
dreaddymck <dreaddymck@users.noreply.github.com>
2021-11-18 12:59:51 -0500

    gif animation added

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 Plugin Name: (DMCK) audio player
 Plugin URI: dreaddymck.com
 Description: Just another media thingy. Can be used to generate playlists and simple charts. Shortcode [dmck-audioplayer]
-Version: 1.1.1-23f723f
+Version: 1.1.1-1663411
 Author: dreaddymck
 Author URI: dreaddymck.com
 License: GPL2
