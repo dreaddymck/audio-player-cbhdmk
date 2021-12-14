5b98439ec9a50adbafa23c6b006d9e39d5f371a4
dreaddymck <dreaddymck@users.noreply.github.com>
2021-12-14 12:02:01 -0500

    rss cache memory limit set

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 Plugin Name: (DMCK) audio player
 Plugin URI: https://github.com/dreaddymck/audio-player-cbhdmk
 Description: Generates a media playlists and simple charts. Shortcode [dmck-audioplayer]
-Version: 1.1.1-3e6f61d
+Version: 1.1.1-3dd4055
 Author: dreaddymck
 Author URI: https://github.com/dreaddymck
 License: GPL2
