0c1a6cd3596ef7c4e1d15960750d175774e103ce
dreaddymck <dreaddymck@users.noreply.github.com>
2022-01-25 15:03:19 -0500

    Known security vulnerabilities
    CVE-2021-23566 Moderate severity

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 Plugin Name: (DMCK) audio player
 Plugin URI: https://github.com/dreaddymck/audio-player-cbhdmk
 Description: Generates a media playlists and simple charts. Shortcode [dmck-audioplayer]
-Version: 1.1.1-a74e25b
+Version: 1.1.1-ef234bc
 Author: dreaddymck
 Author URI: https://github.com/dreaddymck
 License: GPL2
