df2df514d29d377d811cc802b91d05599d980a50
dreaddymck <dreaddymck@users.noreply.github.com>
2022-02-02 16:33:46 -0500

    temp default css

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 Plugin Name: (DMCK) audio player
 Plugin URI: https://github.com/dreaddymck/audio-player-cbhdmk
 Description: Generates a media playlists and simple charts. Shortcode [dmck-audioplayer]
-Version: 1.1.1-b486735
+Version: 1.1.1-3ee54fc
 Author: dreaddymck
 Author URI: https://github.com/dreaddymck
 License: GPL2
