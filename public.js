e03c905c8d030aa8e99e2288a26851dcc0fdecd5
dreaddymck <dreaddymck@users.noreply.github.com>
2022-08-29 16:00:59 -0500

    removed unused

--- public.js
+++ public.js
@@ -1,7 +1,7 @@
 import '@fortawesome/fontawesome-free/js/fontawesome'
 import '@fortawesome/fontawesome-free/js/solid'
-import '@fortawesome/fontawesome-free/js/regular'
-import '@fortawesome/fontawesome-free/js/brands'
+// import '@fortawesome/fontawesome-free/js/regular'
+// import '@fortawesome/fontawesome-free/js/brands'
 import 'pure';
 import 'base';
 import 'grids';
